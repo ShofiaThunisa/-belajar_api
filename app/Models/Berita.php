0bfd5927d910602bf47722836afe503cac7dcd57
thunisashofia@gmail.com <thunisashofia@gmail.com>
2024-08-01 14:44:00 +0700

    pertama

---
+++ app/Models/Berita.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Berita extends Model
+{
+    use HasFactory;
+    protected $fillable = ['judul', 'deskripsi', 'slug', 'foto', 'id_user', 'id_kategori'];
+    public function user (){
+        return $this->belongsTo(Tag::class, 'id_user');
+    }
+    public function kategori (){
+        return $this->belongsTo(Tag::class, 'id_kategori');
+    }
+    public function tag (){
+        return $this->belongsToMany(Tag::class, 'tag_berita', 'id_berita', 'id_tag');
+    }
+}
