8a2c68c9407401f92206ae6e6eebc4f20c4f67f2
thunisashofia@gmail.com <thunisashofia@gmail.com>
2024-08-02 14:21:32 +0700

    satu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
                             Selamat Datang @guest
                         @else
-                            <b>({Auth::use()->user})
+                            <b>({Auth::user()->user})
                             @endguest
                 </div>
             </div>
